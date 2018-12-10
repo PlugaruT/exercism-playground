aba5fc0fa9d1ea53a8a5c0957f8f67ee671dc3d9
Plugaru Tudor <tudor.plugaru@made.com>
2018-12-10 17:35:56 +0200

    Add bob solution in go

---
+++ go/bob/bob.go
@@ -0,0 +1,43 @@
+// Package bob contains all the functions to simulate Bob the teenager
+package bob
+
+import "strings"
+
+func sanitize(str string) string {
+	return strings.TrimSpace(str)
+}
+
+func isUpper(str string) bool {
+	return str == strings.ToUpper(str) && str != strings.ToLower(str)
+}
+
+func isBlank(str string) bool {
+	return sanitize(str) == ""
+}
+
+func isQuestion(str string) bool {
+	return strings.HasSuffix(sanitize(str), "?")
+}
+
+func isYelledSentence(str string) bool {
+	return isUpper(str) && !isQuestion(str)
+}
+
+func isYelledQuestion(str string) bool {
+	return isUpper(str) && isQuestion(str)
+}
+
+// Hey simulates Bob, the lackadaisical teenager
+func Hey(remark string) string {
+	switch {
+	case isYelledSentence(remark):
+		return "Whoa, chill out!"
+	case isYelledQuestion(remark):
+		return "Calm down, I know what I'm doing!"
+	case isQuestion(remark):
+		return "Sure."
+	case isBlank(remark):
+		return "Fine. Be that way!"
+	}
+	return "Whatever."
+}
