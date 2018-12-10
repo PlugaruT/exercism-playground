6d7911407ac2f9a9e029453181010e020d669dc4
Plugaru Tudor <tudor.plugaru@made.com>
2018-12-10 17:40:31 +0200

    loop indexes in go are by default called i :)

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -4,13 +4,13 @@ import "errors"
 
 // Distance receives two strings and computes the hamming distance
 func Distance(a, b string) (int, error) {
-	distance := 0
 	if len(a) != len(b) {
 		return 0, errors.New("The string length should be the same")
 	}
 
-	for index := 0; index < len(a); index++ {
-		if a[index] != b[index] {
+	distance := 0
+	for i := range a {
+		if a[i] != b[i] {
 			distance++
 		}
 	}
