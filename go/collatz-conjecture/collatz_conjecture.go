3ab0fefa3af61bddda708b56ae2db321fe9fedd0
Plugaru Tudor <tudor.plugaru@made.com>
2018-12-11 13:18:48 +0200

    improvements

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,18 +1,14 @@
 package collatzconjecture
 
-import "errors"
+import "fmt"
 
 // CollatzConjecture solves the 3x+1 problem
 func CollatzConjecture(n int) (int, error) {
 	if n <= 0 {
-		return 0, errors.New("Value for n should be greater than 0")
+		return 0, fmt.Errorf("Value for %d should be greater than 0", n)
 	}
 
 	steps := 0
-
-	if n == 1 {
-		return steps, nil
-	}
 
 	for n > 1 {
 		if n%2 == 0 {
