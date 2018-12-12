bcdfc9ba1ec38925be668c4752f38affb9559778
Plugaru Tudor <tudor.plugaru@made.com>
2018-12-12 16:57:57 +0200

    Improve proverb solution

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -5,32 +5,33 @@ import (
 )
 
 func formProverb(first, second string) string {
-	var proverb string
-	if second == "" {
-		proverb = fmt.Sprintf("And all for the want of a %s.", first)
-	} else {
-		proverb = fmt.Sprintf("For want of a %s the %s was lost.", first, second)
+	shortVersion := "And all for the want of a %s."
+	logVersion := "For want of a %s the %s was lost."
+	proverb := fmt.Sprintf(shortVersion, first)
+	if second != "" {
+		proverb = fmt.Sprintf(logVersion, first, second)
 	}
 	return proverb
 }
 
+func nextOrEmpty(arr []string, index int) string {
+	var next = ""
+	if !(len(arr)-1 == index) {
+		next = arr[index+1]
+	}
+	return next
+}
+
 // Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
-	if len(rhyme) == 0 {
-		return []string{}
-	}
 	var response = []string{}
-
 	for i := range rhyme {
 		var curr = rhyme[i]
-		var next = ""
-		if !(len(rhyme)-1 == i) {
-			next = rhyme[i+1]
-		} else {
+		var next = nextOrEmpty(rhyme, i)
+		if len(rhyme)-1 == i {
 			curr = rhyme[0]
 		}
 		response = append(response, formProverb(curr, next))
 	}
-
 	return response
 }
