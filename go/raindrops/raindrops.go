ad5a7f5583000bc3cff69897dfb36a0c1c4c9519
Plugaru Tudor <tudor.plugaru@made.com>
2018-12-14 09:34:45 +0200

    Use string instead of slice

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,6 +1,5 @@
 package raindrops
 
-import "strings"
 import "strconv"
 
 const testVersion = 3
@@ -11,18 +10,18 @@ func isDivBy(nr int, divBy int) bool {
 
 // Convert a number to a string, the contents of which depend on the number's factors.
 func Convert(i int) string {
-	s := make([]string, 0)
+	var result string
 	if isDivBy(i, 3) {
-		s = append(s, "Pling")
+		result += "Pling"
 	}
 	if isDivBy(i, 5) {
-		s = append(s, "Plang")
+		result += "Plang"
 	}
 	if isDivBy(i, 7) {
-		s = append(s, "Plong")
+		result += "Plong"
 	}
-	if len(s) == 0 {
+	if len(result) == 0 {
 		return strconv.Itoa(i)
 	}
-	return strings.Join(s, "")
+	return result
 }
